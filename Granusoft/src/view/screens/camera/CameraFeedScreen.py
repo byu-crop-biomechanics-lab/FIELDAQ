1df37d246096c17d1d14e05ae3d2d18d2784e00a
jaredhmt <jaredhmt@gmail.com>
2020-07-02 14:43:11 -0600

    Undid some changes to capture image that were counterproductive.

--- Granusoft/src/view/screens/camera/CameraFeedScreen.py
+++ Granusoft/src/view/screens/camera/CameraFeedScreen.py
@@ -36,37 +36,11 @@ class CameraFeedScreen(BaseScreen):
         camera.exif_tags['IFD0.Copyright'] = 'Copyright (c) 2020 BYU Crop Biomechanics Laboratory'
     except:
         pass # print('No Operator Data Added')
-    try:
-        def decdeg2dms(dd):
-            dd = abs(dd)
-            minutes,seconds = divmod(dd*3600,60)
-            degrees,minutes = divmod(minutes,60)
-            return (degrees,minutes,seconds)
+
+    def on_pre_enter(self):
         sensor = Sensor()
-        sensor.get_header_data()
-        sensor_data = sensor.get_sensor_data()
-        location = [sensor_data["Location"][0], sensor_data["Location"][1]]
-        latdms = decdeg2dms(location[0])
-        londms = decdeg2dms(location[1])
-    except:
-        # print('Location Data not found')
-        location = [0, 0]
-    try:
-        self.camera.exif_tags['GPS.GPSLatitudeRef'] = 'N' if location[0] > 0 else 'S'
-        self.camera.exif_tags['GPS.GPSLongitudeRef'] = 'E' if location[1] > 0 else 'W'
-        self.camera.exif_tags['GPS.GPSLatitude'] = '%d/1,%d/1,%d/100' % (latdms[0], latdms[1], latdms[2])
-        self.camera.exif_tags['GPS.GPSLongitude'] = '%d/1,%d/1,%d/100' % (londms[0], londms[1], londms[2])
-    except:
-        pass # print('Location Data not added')
-
-    def on_pre_enter(self):
-
-    def on_pre_enter(self):
+        sensor.clear_gps_memory()
         self.event = Clock.schedule_interval(self.check_trig, INTERVAL)
-        self.transition_to_state = "Pause"
-        self.sensor.clear_gps_memory()
-        self.ids['adc_button_text'].text = 'ADC\nValues'
-        self.adc_out = 0
 
     def on_enter(self):
         try:
@@ -75,6 +49,28 @@ class CameraFeedScreen(BaseScreen):
             pass # print('No Camera Found')
 
     def captureImage(self):
+        try:
+            def decdeg2dms(dd):
+                dd = abs(dd)
+                minutes,seconds = divmod(dd*3600,60)
+                degrees,minutes = divmod(minutes,60)
+                return (degrees,minutes,seconds)
+            sensor = Sensor()
+            sensor.get_header_data()
+            sensor_data = sensor.get_sensor_data()
+            location = [sensor_data["Location"][0], sensor_data["Location"][1]]
+            latdms = decdeg2dms(location[0])
+            londms = decdeg2dms(location[1])
+        except:
+            # print('Location Data not found')
+            location = [0, 0]
+        try:
+            self.camera.exif_tags['GPS.GPSLatitudeRef'] = 'N' if location[0] > 0 else 'S'
+            self.camera.exif_tags['GPS.GPSLongitudeRef'] = 'E' if location[1] > 0 else 'W'
+            self.camera.exif_tags['GPS.GPSLatitude'] = '%d/1,%d/1,%d/100' % (latdms[0], latdms[1], latdms[2])
+            self.camera.exif_tags['GPS.GPSLongitude'] = '%d/1,%d/1,%d/100' % (londms[0], londms[1], londms[2])
+        except:
+            pass # print('Location Data not added')
         try:
             dt = datetime.datetime.now()
             filename = 'Images/Stalk_' + dt.strftime('%Y_%m_%d_%H_%M_%S') + '.jpg'
