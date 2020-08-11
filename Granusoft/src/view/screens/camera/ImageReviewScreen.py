f95c69aa592cab9a58cb66bab1bf2e0f6bf24306
JaredHMT <jaredhmt@gmail.com>
2020-08-11 15:14:37 -0600

    When viewing images, a tmp image file is created, displayed, and then deleted in order to show the user the stalk image.

--- Granusoft/src/view/screens/camera/ImageReviewScreen.py
+++ Granusoft/src/view/screens/camera/ImageReviewScreen.py
@@ -26,9 +26,18 @@ Builder.load_file('view/screens/camera/ImageReviewScreen.kv')
 class ImageReviewScreen(BaseScreen):
     image_name = StringProperty()
 
+    def on_enter(self):
+        self.img = self.ids['stalk_img']
+        self.small_img = pili.open(self.image_path)
+        self.small_img.thumbnail((560,460))
+        self.small_img.save(self.img_path, format='JPEG', optimize=True, quality=80)
+        self.img.source = self.img_path
+        os.system("rm " + self.img_path)
+
     def set_image(self, name):
         self.image_name = name
         self.image_path = "Images/" + name
+        self.img_path = "Images/tmp_" + name
 
     def delete_button(self):
         print('We should delete image ' + self.image_name)
